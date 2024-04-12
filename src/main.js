708c9b03afd50cd9417f10d36abaa9de579f3e40
afsds <142832572+afsds@users.noreply.github.com>
2024-04-12 15:52:52 -0300

    Delete src/main.js

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Calculator from './components/Calculator.vue'
-
-createApp(App)
-  .component('calculator', Calculator)
-  .mount('#app')
-
